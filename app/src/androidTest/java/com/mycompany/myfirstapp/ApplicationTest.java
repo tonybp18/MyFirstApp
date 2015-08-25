43f37358e9c77e756076ba515bf37a2d88be929e
Tony B <tonybp18@gmail.com>
2015-08-24 21:55:44 -0430

    Primer Commit

---
+++ app/src/androidTest/java/com/mycompany/myfirstapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.mycompany.myfirstapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <awrap_content href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
