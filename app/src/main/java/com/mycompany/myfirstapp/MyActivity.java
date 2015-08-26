b37940af8716541a2464e5cffa58e0b5e0e4f80c
Tony Briceño <tonybp18@gmail.com>
2015-08-26 18:17:15 -0430

    Realización del ejemplo de "adding the action bars: Adding action buttons" -- Siguiente curso: "adding the action bars: styling the action bar" http://developer.android.com/training/basics/actionbar/styling.html

--- app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
+++ app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
@@ -3,10 +3,12 @@ package com.mycompany.myfirstapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -21,8 +23,13 @@ public class MyActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
+        /*getMenuInflater().inflate(R.menu.menu_my, menu);
         return true;
+        /**/
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -30,6 +37,7 @@ public class MyActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        /**/
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -38,14 +46,32 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+        /*
+        switch (item.getItemId()){
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        /**/
     }
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        String message = editText.getText().toString().trim();
+
+        if( !message.isEmpty() ) {
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        }else{
+            Toast toast = Toast.makeText(this, "Debe ingresar texto en el campo.", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
